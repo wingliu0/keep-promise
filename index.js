3cd07022df9339f352034f9086047e2a16b01af5
Wing Liu <wing@cxinno.tw>
2018-11-24 01:19:11 +0800

    致敬

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-var keepPromise = function (promiseBasedFunction) {
-  if (typeof promiseBasedFunction !== 'function') {
-    throw 'please pass in function'
-  }
+const kCustomPromisifiedSymbol = Symbol('keepPromise.custom');
 
-  return function () {
-    if (arguments.length - promiseBasedFunction.length === 1) {
-      var cb = arguments[arguments.length - 1]
-      if (typeof cb === 'function') {
-        var args = Array.from(arguments)
-        args.pop()
-        promiseBasedFunction(args).then(function (r) {
-          cb(null, r)
-        }).catch(function (e) {
-          cb(e)
-        })
-        return null
-      }
+function keepPromise(orig) {
+  if (typeof orig !== 'function')
+    throw new TypeError('please pass in function')
+
+  if (orig[kCustomPromisifiedSymbol]) {
+    const fn = orig[kCustomPromisifiedSymbol]
+    if (typeof fn !== 'function') {
+      throw new TypeError('The [util.promisify.custom] property must be ' +
+        'a function');
     }
-    return promiseBasedFunction(arguments)
+    Object.defineProperty(fn, kCustomPromisifiedSymbol, {
+      value: fn, enumerable: false, writable: false, configurable: true
+    });
+    return fn;
   }
+
+
+  function fn(...args) {
+    try {
+      if (args.length > 0 && typeof args[args.length - 1] === 'function') {
+        const cb = args.pop()
+        orig.call(this, ...args).then(_ => cb(null, _)).catch(_ => cb(_))
+        return null
+      } else {
+        return orig.call(this, ...args)
+      }
+    } catch (error) {
+      return Promise.reject(new Error('fail'))
+    }
+  }
+
+  Object.setPrototypeOf(fn, Object.getPrototypeOf(orig));
+
+  Object.defineProperty(fn, kCustomPromisifiedSymbol, {
+    value: fn, enumerable: false, writable: false, configurable: true
+  });
+  return Object.defineProperties(fn, Object.getOwnPropertyDescriptors(orig));
 }
-module.exports = keepPromise
-
-
